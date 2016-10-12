03ba7b416b6fe2b6408dc37790d08cdfd3040e28
robertofcfm <robertofcfm@hotmail.com>
2016-10-11 23:37:38 -0500

    v0.2.1 Add The Network Call Code
    + se agrega el código que se conecta al servicio web para obtener la
    información de los restaurantes y lo guarda en una cadena con formato
    json

--- app/src/main/java/com/herroj/android/lunchtimefrontend/app/RestaurantMainActivity.java
+++ app/src/main/java/com/herroj/android/lunchtimefrontend/app/RestaurantMainActivity.java
@@ -3,6 +3,7 @@ package com.herroj.android.lunchtimefrontend.app;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -71,7 +80,7 @@ public class RestaurantMainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
-        ArrayAdapter<String> mForecastAdapter;
+        ArrayAdapter<String> mRestaurantAdapter;
 
         public PlaceholderFragment() {
         }
@@ -109,7 +118,7 @@ public class RestaurantMainActivity extends ActionBarActivity {
             // Now that we have some dummy forecast data, create an ArrayAdapter.
             // The ArrayAdapter will take data from a source (like our dummy forecast) and
             // use it to populate the ListView it's attached to.
-            mForecastAdapter =
+            mRestaurantAdapter =
                     new ArrayAdapter<String>(
                             getActivity(), // The current context (this activity)
                             R.layout.list_item_restaurant, // The name of the layout ID.
@@ -122,7 +131,80 @@ public class RestaurantMainActivity extends ActionBarActivity {
 
             // 1.06 Get a reference to the ListView, and attach this adapter to it.
             ListView listView = (ListView) rootView.findViewById(R.id.listview_restaurant);
-            listView.setAdapter(mForecastAdapter);
+            listView.setAdapter(mRestaurantAdapter);
+
+            /*
+                RHR
+                2.01 Add the network call code
+
+                Nota: es normal que no corra la aplicacion en este punto, hay que hacer algo mas
+            */
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String restaurantJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                String baseUrl = "http://robertofcfm.mooo.com:8080/LunchTimeBackend/webresources/com.herroj.lunchtimebackend.restaurant";
+                URL url = new URL(baseUrl);
+                //String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                //String apiKey = "&APPID=" + "\"9df3e8a2ea20a727bb82439b8aa424cf\"";
+                //URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                restaurantJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            }  finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            // Fin 2.01
 
             return rootView;
         }
