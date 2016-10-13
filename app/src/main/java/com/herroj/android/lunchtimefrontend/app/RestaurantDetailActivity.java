2a1ecc234ddedef122598defc78ab79c3c4248f0
robertofcfm <robertofcfm@hotmail.com>
2016-10-13 00:53:28 -0500

    v0.3.12 Add ShareActionProvider To Detail Fragment Menu
    + se agrega la opcion de compartir en el menu de detalle

--- app/src/main/java/com/herroj/android/lunchtimefrontend/app/RestaurantDetailActivity.java
+++ app/src/main/java/com/herroj/android/lunchtimefrontend/app/RestaurantDetailActivity.java
@@ -2,11 +2,15 @@ package com.herroj.android.lunchtimefrontend.app;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -56,7 +60,13 @@ public class RestaurantDetailActivity extends ActionBarActivity {
      */
     public static class RestaurantDetailFragment extends Fragment {
 
+        private static final String LOG_TAG = RestaurantDetailFragment.class.getSimpleName();
+
+        private static final String FORECAST_SHARE_HASHTAG = " #LunchTimeApp";
+        private String mRestaurantStr;
+
         public RestaurantDetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -65,16 +75,47 @@ public class RestaurantDetailActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
 
-                        // The detail Activity called via intent.  Inspect the intent for forecast data.
-                                Intent intent = getActivity().getIntent();
-                        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                                ((TextView) rootView.findViewById(R.id.restaurant_detail_text))
-                                               .setText(forecastStr);
-                            }
+            // The detail Activity called via intent.  Inspect the intent for forecast data.
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                mRestaurantStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.restaurant_detail_text))
+                        .setText(mRestaurantStr);
+            }
 
             return rootView;
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.restaurantdetailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mRestaurantStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
     }
 }
 
