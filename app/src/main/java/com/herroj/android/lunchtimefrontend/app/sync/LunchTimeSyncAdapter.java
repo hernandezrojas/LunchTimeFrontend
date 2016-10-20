94d49a03da9b4e3b1cc4b25c2dbdbde1c83fd802
robertofcfm <robertofcfm@hotmail.com>
2016-10-19 21:11:12 -0500

    v0.6.7 Notifications Answer
    + programacion de respuesta notificaciones

--- app/src/main/java/com/herroj/android/lunchtimefrontend/app/sync/LunchTimeSyncAdapter.java
+++ app/src/main/java/com/herroj/android/lunchtimefrontend/app/sync/LunchTimeSyncAdapter.java
@@ -2,11 +2,14 @@ package com.herroj.android.lunchtimefrontend.app.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SyncRequest;
 import android.content.SyncResult;
@@ -15,9 +18,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.herroj.android.lunchtimefrontend.app.R;
+import com.herroj.android.lunchtimefrontend.app.RestaurantMainActivity;
 import com.herroj.android.lunchtimefrontend.app.Utility;
 import com.herroj.android.lunchtimefrontend.app.data.RestaurantContract;
 import com.herroj.android.lunchtimefrontend.app.data.RestaurantContract.RestaurantEntry;
@@ -252,6 +258,8 @@ public class LunchTimeSyncAdapter extends AbstractThreadedSyncAdapter {
                 String horaApertura = cursor.getString(INDEX_HORA_APERTURA);
                 String horaCierre = cursor.getString(INDEX_HORA_CIERRE);
 
+                int iconId = R.drawable.ic_logo;
+                String title = context.getString(R.string.app_name);
 
                 // Define the text of the forecast.
                 String contentText = String.format(context.getString(R.string.format_notification),
@@ -259,7 +267,35 @@ public class LunchTimeSyncAdapter extends AbstractThreadedSyncAdapter {
                         horaApertura,
                         horaCierre);
 
-                //build your notification here.
+                // NotificationCompatBuilder is a very convenient way to build backward-compatible
+                // notifications.  Just throw in some data.
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(getContext())
+                                .setSmallIcon(iconId)
+                                .setContentTitle(title)
+                                .setContentText(contentText);
+
+                // Make something interesting happen when the user clicks on the notification.
+                // In this case, opening the app is sufficient.
+                Intent resultIntent = new Intent(context, RestaurantMainActivity.class);
+
+                // The stack builder object will contain an artificial back stack for the
+                // started Activity.
+                // This ensures that navigating backward from the Activity leads out of
+                // your application to the Home screen.
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(
+                                0,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                mBuilder.setContentIntent(resultPendingIntent);
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                // WEATHER_NOTIFICATION_ID allows you to update the notification later on.
+                mNotificationManager.notify(RESTAURANT_NOTIFICATION_ID, mBuilder.build());
 
                 //refreshing last sync
                 SharedPreferences.Editor editor = prefs.edit();
